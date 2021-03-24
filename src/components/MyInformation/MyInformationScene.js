11520bc8c0e04c8bc9e107116d2b0e244472ce3f
Elena <sir.elena.lvaose@gmail.com>
2021-03-24 11:47:42 -0500

    add button and prompt for my info

--- src/components/MyInformation/MyInformationScene.js
+++ src/components/MyInformation/MyInformationScene.js
@@ -1,10 +1,36 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button, StyleSheet, Alert } from 'react-native';
 
 export default function MyInformationScene() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>My Information!</Text>
+    <View style={styles.container}>
+      <Text  style={styles.title}>
+        My Information
+      </Text>
+      <Text  style={styles.prompt}>
+        Provide information about yourself that will be helpful to first responders.
+      </Text>
+      <Button title="Begin"
+              onPress={() => Alert.alert('Simple Button pressed')} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 50,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 36
+  },
+  prompt: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 24
+  }
+})
